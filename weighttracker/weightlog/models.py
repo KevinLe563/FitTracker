dbe18fb0ac141930435ba3ee24160b472ff9ed82
Kevin Le <k26le@uwaterloo.ca>
2022-03-28 21:03:17 -0600

    Setup weight model

--- weighttracker/weightlog/models.py
+++ weighttracker/weightlog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Weight(models.Model):
+    # Each weight is associated with a user
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateField(null=False, blank=False)
+    note = models.TextField(max_length=500, help_text="Personal notes")
+
+    def __str__(self):
+        """Represent weight object as a date"""
+        return self.date
